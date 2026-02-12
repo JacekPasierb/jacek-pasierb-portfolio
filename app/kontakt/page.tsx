4b580fcd7a6081d08db8cd583db875818ebda633
Jacek Pasierb <jack_byk@o2.pl>
2026-02-12 02:08:07 +0100

    Fix - add load for i18n.ts

--- app/kontakt/page.tsx
+++
@@ -1,9 +0,0 @@
-import {redirect} from "next/navigation";
-
-/**
- * Kontakt jest sekcją na stronie głównej (#kontakt).
- * Przekierowanie zachowuje działanie starych linków i bookmarks.
- */
-export default function KontaktPage() {
-  redirect("/#kontakt");
-}
